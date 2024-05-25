22a6ef1744cbd22ad5ccd5e9eda7083107f9760e
LDraco-Sharif <shariful.islam.msi47@gmail.com>
2024-05-26 01:14:29 +0600

    Added a basic page

---
+++ src/app/components/demo-page/demo-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './demo-page.component.html',
+  styleUrl: './demo-page.component.css'
+})
+export class DemoPageComponent {
+
+}
