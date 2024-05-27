6b94e9f24a4246338f812ba36860119ede7dd1d7
LDraco-Sharif <shariful.islam.msi47@gmail.com>
2024-05-28 02:00:47 +0600

    Updated education page

--- src/app/components/demo-page/demo-page.component.ts
+++ src/app/components/demo-page/demo-page.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
-
+import { MatTabsModule } from '@angular/material/tabs';
+import { EducationComponent } from "../education/education.component";
 @Component({
-  selector: 'app-demo-page',
-  standalone: true,
-  imports: [],
-  templateUrl: './demo-page.component.html',
-  styleUrl: './demo-page.component.css'
+    selector: 'app-demo-page',
+    standalone: true,
+    templateUrl: './demo-page.component.html',
+    styleUrl: './demo-page.component.css',
+    imports: [
+        MatTabsModule,
+        EducationComponent
+    ]
 })
 export class DemoPageComponent {
 
